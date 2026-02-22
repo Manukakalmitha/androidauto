4637a3a7650ce38fe523bd49ae25ad6b47090331
Manukakalmitha <manukakalmitha@gmail.com>
2026-02-22 13:59:59 +0530

    feat: initialize new Android Trusted Web Activity project structure.

---
+++ app/src/main/java/com/manuk/androidauto/DelegationService.java
@@ -0,0 +1,14 @@
+package com.manuk.androidauto;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
